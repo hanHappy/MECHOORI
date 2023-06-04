7b0abd5efdb7720d405ff82b2c9d08efce3080c3
hanHappy <sangmin950706@gmail.com>
2023-06-05 00:15:24 +0900

    평가 페이지 모달(평가완료 +이전으로 + 홈으로) 제작 완료

--- js/rate.js
+++ js/rate.js
@@ -1,39 +1,57 @@
-window.addEventListener("load", function () {
-  // 슬라이더 --------------------------------------
-  let slider = document.querySelector("#my-range");
-  let output = document.querySelector("#demo");
+// 슬라이더 --------------------------------------
+let slider = document.querySelector("#my-range");
+let output = document.querySelector("#demo");
 
-  // 슬라이더 조작 -> input 값 업데이트
-  slider.oninput = function () {
-    output.value = this.value;
+// 슬라이더 조작 -> input 값 업데이트
+slider.oninput = function () {
+  output.value = this.value;
+}
+
+// 평가 확인 모달 ------------------------------------------
+let rateBtn = document.querySelector('.rate-btn');
+let backBtn = document.querySelector('.header-icon-back');
+let homeBtn = document.querySelector('.header-icon-home');
+let modalComplete = document.querySelector('.modal.check-complete');
+let modalBack = document.querySelector('.modal.check-back');
+let modalHome = document.querySelector('.modal.check-home');
+let no = this.document.querySelectorAll('.no');
+
+let closeModal = function(){
+  if (modalComplete.style.display == 'block')
+    modalComplete.style.display = 'none';
+  else if (modalBack.style.display == 'block')
+    modalBack.style.display = 'none';
+  else if (modalHome.style.display == 'block')
+    modalHome.style.display = 'none';
+}
+
+window.addEventListener('click', function(e) {
+  switch(e.target){
+    case backBtn:
+      modalBack.style.display = 'block';
+      break;
+    case homeBtn:
+      modalHome.style.display = 'block';
+      break;
+    case rateBtn:
+      modalComplete.style.display = 'block';
+      break;
   }
-
-  // 평가 확인 모달 ------------------------------------------
-  let rateBtn = document.querySelector('#rate-btn');
-  let modal = document.querySelector('.modal.check-complete');
-  let no = this.document.querySelector('.no');
-
-  // 모달 열기
-  function openModal() {
-    modal.style.display = 'block';
-  }
-
-  // 모달 닫기 (아니오 클릭 시)
-  function closeModal() {
-    modal.style.display = 'none';
-  }
-
-  // 모달 닫기 (모달 외부 클릭 시)
-  window.addEventListener('click', function (e) {
-    if (e.target!=modal)
-      return;
-    closeModal();
-  })
-
-  // 평가 버튼 -> 모달 열기
-  rateBtn.onclick = openModal;
-
-  // 닫기 버튼 -> 모달 닫기
-  no.onclick = closeModal;
-
 });
+
+// 모달 영역 밖 클릭 시 모달 닫기
+window.addEventListener('click', function(e) {
+  let isModal = true;
+  if (e.target == modalComplete 
+    || e.target == modalBack 
+    || e.target == modalHome)
+    isModal = false;
+    
+    if(!isModal) {
+      e.target.style.display = 'none';
+    }
+  });
+
+  // 아니오 버튼 클릭시 모달 닫기
+  for(let i = 0; i < no.length; i++)
+    no[i].addEventListener('click', closeModal);
