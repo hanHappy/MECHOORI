10918267a3793570d98c2b6a466b6561d510b070
seyoungheo <syheo925@gmail.com>
2023-06-09 11:56:23 +0900

    카테고리 기능 추가

--- src/main/java/com/mechoori/web/controller/RestaurantController.java
+++ src/main/java/com/mechoori/web/controller/RestaurantController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mechoori.web.entity.Category;
 import com.mechoori.web.entity.Menu;
 import com.mechoori.web.entity.Restaurant;
+import com.mechoori.web.service.CategoryService;
 import com.mechoori.web.service.RestaurantService;
 
 @Controller
@@ -19,15 +21,26 @@ public class RestaurantController {
 
 	@Autowired
 	private RestaurantService service;
+	@Autowired
+	private CategoryService ctgservice;
+	
 
+	// /restaurant/list/category/{id}(id=${c.id})}
 	@GetMapping("/list/category/{id}")
 	public String list(
 			@PathVariable("id") int categoryId,
 			Model model) {
 
+		//헤더에 식당 카테고리 출력
+		Category category = ctgservice.getDetail(categoryId);
+		//식당 리스트 출력
 		List<Restaurant> list = service.getList(categoryId);
+		//식당 리스트 평균가격 출력
+		
+		
 
-		model.addAttribute("list", list);
+		model.addAttribute("list", list)
+			 .addAttribute("category", category);
 
 		return "restaurant/category";
 	}
