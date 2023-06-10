559ff66e50deef54175fab836441f8e444e83dfc
hanHappy <sangmin950706@gmail.com>
2023-06-10 23:14:24 +0900

    header 가이드에 맞춰  수정 중

--- src/test/java/com/mechoori/web/repository/RestaurantRepositoryTest.java
+++ src/test/java/com/mechoori/web/repository/RestaurantRepositoryTest.java
@@ -1,12 +1,11 @@
 package com.mechoori.web.repository;
 
-import com.mechoori.web.entity.Menu;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
-import com.mechoori.web.entity.Restaurant;
+import com.mechoori.web.entity.Menu;
 
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
