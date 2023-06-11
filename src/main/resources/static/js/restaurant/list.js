aba5448098f1f5095590fbef373ec1ce182ff95f
hanHappy <sangmin950706@gmail.com>
2023-06-11 21:12:15 +0900

    restaurant/list 페이지 90 완
    (TODO)
    - 카테고리, 필터 블럭 고정

--- src/main/resources/static/js/restaurant/list.js
+++ src/main/resources/static/js/restaurant/list.js
@@ -1,16 +1,38 @@
-window.addEventListener('load', function () {
+let likeBtns = this.document.querySelectorAll('.like');
+let tags = this.document.querySelectorAll('.category-tag');
+let categoryBtns = document.querySelectorAll('.rs-menu');
+let otherCategories = document.querySelector('.category.others');
+let categoryOthersContainer = document.querySelector('.category-others-container');
 
-    let likeBtn = this.document.querySelectorAll(".like");
+let likeControl = function (e) {
+    let isLiked = e.target.classList.contains("active");
+    if (isLiked)
+        e.target.classList.remove('active');
+    else
+        e.target.classList.add('active');
+}
 
-    let likeControl = function (e) {
-        let isLiked = !(e.target.classList.contains("inactive"));
-        if (isLiked)
-            e.target.classList.add("inactive");
-        else
-            e.target.classList.remove("inactive");
-    }
+let tagControl = function (e){
+    let selected = e.target.classList.contains("active");
+    if(selected)
+        e.target.classList.remove('active');
+    else
+        e.target.classList.add('active');
+}
 
-    for(let i = 0; i < likeBtn.length; i++)
-        likeBtn[i].onclick = likeControl;
+let categoryControl = function(e){
+    let isOthers = e.target.classList.contains("others");
+    if(isOthers)
+        categoryOthersContainer.classList.add('slide-open');
+    else
+        categoryOthersContainer.classList.remove('slide-open');
+}
 
-})
+for (let btn of likeBtns)
+    btn.onclick = likeControl;
+
+for (let tag of tags)
+    tag.onclick = tagControl;
+
+for(let btn of categoryBtns)
+    btn.onclick = categoryControl;
