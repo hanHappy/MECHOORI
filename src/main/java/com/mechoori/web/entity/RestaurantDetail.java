b4b16f66c8f32c32e96a0183e9ca239a2984b9d2
hanHappy <sangmin950706@gmail.com>
2023-06-13 10:52:44 +0900

    restaurant/detail 페이지 식당 상세(평균 가격, 평가 가격)부분 수정

--- src/main/java/com/mechoori/web/entity/RestaurantDetail.java
+++ src/main/java/com/mechoori/web/entity/RestaurantDetail.java
@@ -19,5 +19,6 @@ public class RestaurantDetail {
     private Integer likedCount;
     private Integer ratedCount;
     private Integer avgPrice;
+    private Integer avgRatedPrice;
     private Integer value;
 }
