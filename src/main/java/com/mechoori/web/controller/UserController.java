fe3ecc27038ec4ad609288f53981a73bf5a495dd
hanHappy <sangmin950706@gmail.com>
2023-06-15 13:16:34 +0900

    회원가입 -> 멤버 객체로 받아오기 성공
    (TODO)
    - form 페이지에 정보 추가로 받아야 함

--- src/main/java/com/mechoori/web/controller/UserController.java
+++ src/main/java/com/mechoori/web/controller/UserController.java
@@ -2,7 +2,10 @@ package com.mechoori.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mechoori.web.entity.Member;
 
 @Controller
 @RequestMapping("user")
@@ -13,7 +16,19 @@ public class UserController {
     }
 
     @GetMapping("sign-up/policy")
-    public String signUp(){
+    public String policy(){
         return "user/sign-up/policy";
     }
+
+    @GetMapping("sign-up/form")
+    public String form(){
+        return "user/sign-up/form";
+    }
+
+    @PostMapping("sign-up/form")
+    public String form(Member member){
+        System.out.println(member);
+        return "redirect:complete";
+    }
+
 }
