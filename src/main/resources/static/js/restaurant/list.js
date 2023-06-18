af07cd8104875756bf51d9f9ebb5355da5264ff9
hanHappy <sangmin950706@gmail.com>
2023-06-18 17:50:36 +0900

    feat : restaurant/list 검색바 추가
    (TODO)
    1. index에서 검색으로 넘어왔을 땐 검색바가, 카테고리 검색으로 넘어왔을 땐 헤더가 보이도록 해야 함

--- src/main/resources/static/js/restaurant/list.js
+++ src/main/resources/static/js/restaurant/list.js
@@ -3,7 +3,22 @@ let topCategoryNav = document.querySelector('.top-category-nav');
 let categoryOthersContainer = document.querySelector('.category-others-container');
 let otherCategories = categoryOthersContainer.querySelector('.others');
 let tagArea = document.querySelector('.category.others');
+let header = document.querySelector('header');
+let headerSide = header.querySelectorAll('.header-side');
+let logo = header.querySelector('header>a');
+let searchContainer = header.querySelector('.search-container');
 
+header.onclick = function(e){
+    if(!e.target.className.includes('search')){
+        return;
+    }
+    headerSide[0].style.display = 'none';
+    headerSide[1].style.display = 'none';
+    logo.style.display = 'none';
+    searchContainer.classList.remove('d-none');
+}
+
+// 좋아요 버튼
 let likeControl = function (e) {
     let isLiked = e.target.classList.contains("active");
     if (isLiked)
@@ -14,6 +29,7 @@ let likeControl = function (e) {
 for (let btn of likeBtns)
     btn.onclick = likeControl;
 
+// 기타 태그 영역
 tagArea.onclick = function (e) {
     e.preventDefault();
     if(e.target.tagName !== 'BUTTON')
@@ -24,6 +40,7 @@ tagArea.onclick = function (e) {
     e.target.classList.add('active');
 };
 
+// Top Category 영역
 topCategoryNav.onclick = function(e){
     e.preventDefault();
     if(e.target.tagName !== 'A')
