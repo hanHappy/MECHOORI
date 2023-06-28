122db487ea69436c9cee5b1bbe4a3591f824c2a7
seyoungheo <syheo925@gmail.com>
2023-06-28 10:46:02 +0900

    feat: api/usercontroller 수정

--- src/main/java/com/mechoori/web/api/controller/UserController.java
+++ src/main/java/com/mechoori/web/api/controller/UserController.java
@@ -1,19 +1,35 @@
 package com.mechoori.web.api.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ViewResolver;
 
-import com.mechoori.web.api.service.UserService;
+//import com.mechoori.web.api.service.UserService;
 
-@Controller("apiUserController")
+@RestController("apiUserController")
 @RequestMapping("api/users")
 public class UserController {
     
-    private UserService userService;
+
+    @Autowired
+    private ViewResolver viewResolver;
 
     @GetMapping("/login")
-    public String login() {
-        return "user/login/social-login";
+    public ModelAndView login() throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setView(viewResolver.resolveViewName("user/login/social-login", Locale.getDefault()));
+        return modelAndView;
     }
+    //private UserService userService;
+
+    // @GetMapping("/login")
+    // public String login() {
+    //     return "user/login/social-login";
+    // }
 }
