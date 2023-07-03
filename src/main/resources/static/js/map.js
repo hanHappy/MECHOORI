1a769ac3fb5db891163aa261cbba0f321a8dea7b
KHC <wkqkrla12@gmail.com>
2023-07-03 17:40:07 +0900

    줌인아웃 맵 스ì뷰 기능 추가

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,55 +1,60 @@
-window.addEventListener("load", function () {
+window.addEventListener("DOMContentLoaded", function () {
     const addresses = document.querySelectorAll(".addresses");
     const resNames = document.querySelectorAll(".resName");
     const contactNumbers = document.querySelectorAll(".contactNumber");
+    const resIds = document.querySelectorAll("#resId");
+
     let markers = [];
 
-    var currentInfoWindow = null; // 현재 열려있는 인포윈도우 추적 변수
+    var currentInfoWindow = null; // Variable to track the currently open info window
 
     for (let i = 0; i < addresses.length; i++) {
         const name = resNames[i].textContent;
         const address = addresses[i].textContent;
         const contactNumber = contactNumbers[i].textContent;
-        markers.push({ name, address, contactNumber });
+        const id = resIds[i].textContent;
+        markers.push({ name, address, contactNumber, id });
     }
 
     kakao.maps.load(function () {
-        var container = document.getElementById('map');
+        var container = document.getElementById("map");
         var options = {
             center: new kakao.maps.LatLng(37.55265499581108, 126.93762683368536),
-            level: 1
+            level: 1,
         };
 
         var map = new kakao.maps.Map(container, options);
         var geocoder = new kakao.maps.services.Geocoder();
 
         markers.forEach(function (markerInfo) {
-            const { name, address, contactNumber } = markerInfo;
+            const { name, address, contactNumber, id } = markerInfo;
 
             geocoder.addressSearch(address, function (result, status) {
                 if (status === kakao.maps.services.Status.OK) {
                     var markerImage = new kakao.maps.MarkerImage(
-                        'https://imgur.com/vSOP7KD.png',
-                        new kakao.maps.Size(31, 35)
+                        "https://imgur.com/vSOP7KD.png",
+                        new kakao.maps.Size(60, 70)
                     );
 
                     var marker = new kakao.maps.Marker({
                         position: new kakao.maps.LatLng(result[0].y, result[0].x),
-                        image: markerImage
+                        image: markerImage,
                     });
 
                     var infowindow = new kakao.maps.InfoWindow({
-                        content: `<div style="width: 100%; height: 100%;background-color:white; border: 1px solid black "><strong>${name}</strong><br>주소: ${address}<br>전화번호: ${contactNumber}</div>`,
-                        maxWidth: 500
+                        maxWidth: 500,
                     });
 
-                    kakao.maps.event.addListener(marker, 'click', function () {
+                    // Set infowindow content outside the geocoder callback
+                    infowindow.setContent(`<a href="restaurant/${id}" style="color: black"><div style="width: 100%; height: 100%;background-color:white; border: 1px solid black; top:15px"><strong>${name}</strong><br>주소: ${address}<br>연락처: ${contactNumber}</div></a>`);
+
+                    kakao.maps.event.addListener(marker, "click", function () {
                         if (currentInfoWindow === infowindow) {
-                            // 현재 열려있는 인포윈도우를 닫음
+                            // Close the currently open info window
                             infowindow.close();
                             currentInfoWindow = null;
                         } else {
-                            // 새로운 인포윈도우를 열고 현재 열려있는 인포윈도우를 업데이트
+                            // Open a new info window and update the currently open info window
                             if (currentInfoWindow) {
                                 currentInfoWindow.close();
                             }
@@ -58,25 +63,57 @@ window.addEventListener("load", function () {
                         }
                     });
 
-                    kakao.maps.event.addListener(marker, 'touchstart', function () {
+                    kakao.maps.event.addListener(marker, "touchstart", function () {
                         infowindow.open(map, marker);
                     });
 
-                    kakao.maps.event.addListener(marker, 'mouseout', function () {
+                    kakao.maps.event.addListener(marker, "mouseout", function () {
                         infowindow.close();
                     });
 
                     marker.setMap(map);
                 }
             });
+
         });
 
-        // 지도를 클릭했을 때 기존에 열려있는 인포윈도우를 닫음
-        kakao.maps.event.addListener(map, 'click', function () {
+        // Close the currently open info window when the map is clicked
+        kakao.maps.event.addListener(map, "click", function () {
             if (currentInfoWindow) {
                 currentInfoWindow.close();
                 currentInfoWindow = null;
             }
         });
+        function zoomIn() {
+            map.setLevel(map.getLevel() - 1);
+        }
+        function zoomOut() {
+            map.setLevel(map.getLevel() + 1);
+        }
+        function setMapType(maptype) {
+            var roadmapControl = document.getElementById('btnRoadmap');
+            var skyviewControl = document.getElementById('btnSkyview');
+            if (maptype === 'roadmap') {
+                map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+                roadmapControl.className = 'selected_btn';
+                skyviewControl.className = 'btn';
+            } else {
+                map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+                skyviewControl.className = 'selected_btn';
+                roadmapControl.className = 'btn';
+            }
+        }
+
+        document.querySelector(".zoomIn").addEventListener("click", zoomIn);
+        document.querySelector(".zoomOut").addEventListener("click", zoomOut);
+
+        document.getElementById('btnRoadmap').addEventListener('click', function() {
+            setMapType('roadmap');
+        });
+
+        document.getElementById('btnSkyview').addEventListener('click', function() {
+            setMapType('skyview');
+        });
+
     });
 });
