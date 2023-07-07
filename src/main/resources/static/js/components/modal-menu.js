06c4c17d4510f35f84827c033233ad0164354439
hanHappy <sangmin950706@gmail.com>
2023-07-07 23:36:40 +0900

    Feat : my-page 모달화 작업중
    (DONE) 패널 너비 조정함
    (TODO) modal constructor shadow 만들자

--- src/main/resources/static/js/components/modal-menu.js
+++ src/main/resources/static/js/components/modal-menu.js
@@ -1,10 +1,31 @@
 class ModalMenuElement extends HTMLElement {
     constructor(){
         let style = `
+            .screen {
+                background-color: transparent;
+            
+                position: fixed;
+                top: 0;
+                left: 0;
+            
+                box-sizing: border-box;
+                width: 100vw;
+                height: 100vh;
+                padding-left: 60px;
+            }
+            
+            .panel {
+                box-shadow: 0 0 10px 3px #75757520;
+            
+                min-width: 315px;
+                height: 100vh;
+            }
+            
             .profile-image-wrapper {
                 background-color: bisque;
             
-                width: 100vw;
+                box-sizing: border-box;
+                width: 100%;
                 height: 200px;
             
                 display: flex;
@@ -105,51 +126,65 @@ class ModalMenuElement extends HTMLElement {
             .icon-power {
                 background-image: url("/images/icons/power.svg");
             }
-            `
+        `
         let template = `
-            <div class="profile-image-wrapper">
-                <div class="profile-image-frame">
-                    <img class="profile-image" src="/images/foods/678버거.jpg"  alt="프로필사진">
-                </div>
-                <div class="nickname-wrapper">
-                    <span class="nickname" th:text="${m.nickname}">메추리</span>
+            <div class="screen">
+                <div class="panel">
+                    <div class="profile-image-wrapper">
+                        <div class="profile-image-frame">
+                            <img class="profile-image" src="/images/foods/678버거.jpg"  alt="프로필사진">
+                        </div>
+                        <div class="nickname-wrapper">
+                            <span class="nickname" th:text="${m.nickname}">메추리</span>
+                        </div>
+                    </div>
+
+                    <nav class="menu-list-section">
+                        <ul>
+                            <li>
+                                <a href="my-page/edit-info">
+                                    <div class="icon icon-key"></div>
+                                    <span>회원 정보 수정</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="my-page/like-list">
+                                    <div class="icon icon-heart"></div>
+                                    <span>찜 목록</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="my-page/rate-list">
+                                    <div class="icon icon-review"></div>
+                                    <span>평가 목록</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="my-page/statistics">
+                                    <div class="icon icon-chart"></div>
+                                    <span>가성비 평가 통계</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/logout">
+                                    <div class="icon icon-power"></div>
+                                    <span>로그아웃</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
-
-            <nav class="menu-list-section">
-                <ul>
-                    <li>
-                        <a href="my-page/edit-info">
-                            <div class="icon icon-key"></div>
-                            <span>회원 정보 수정</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="my-page/like-list">
-                            <div class="icon icon-heart"></div>
-                            <span>찜 목록</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="my-page/rate-list">
-                            <div class="icon icon-review"></div>
-                            <span>평가 목록</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="my-page/statistics">
-                            <div class="icon icon-chart"></div>
-                            <span>가성비 평가 통계</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/logout">
-                            <div class="icon icon-power"></div>
-                            <span>로그아웃</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
         `
-    }
+    } // constructor
+
+    
+    show(status){
+        if(status)
+            this.classList.add("show");
+        else
+            this.classList.remove("show");
+    }   
 }
+
+customElements.define("modal-menu", ModalMenuElement);
