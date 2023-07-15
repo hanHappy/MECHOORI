cbd26a1790d3d9b961db09a664c22f95fa063e45
seyoungheo <syheo925@gmail.com>
2023-07-15 16:13:54 +0900

    feat: /user/* 반응형 웹 적용 중

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,17 +5,18 @@ window.onload = function(){
 }
 
 function slideShow() {
-    let x = document.getElementsByClassName("rateAd");  //slide1에 대한 dom 참조
-    // const con = document.querySelectorAll("adBox");
-    // const x = document.querySelector('rateAd');
-    for (let i=0; i < x.length; i++) {
-        x[i].style.display = "none";   //처음에 전부 display를 none으로 한다.
-    }
+  let x = document.getElementsByClassName("rateAd");  //slide1에 대한 dom 참조
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";   //처음에 전부 display를 none으로 한다.
+  }
+
+  if (!window.matchMedia("(min-width: 1024px)").matches) {
     index++;
     if (index > x.length) {
-        index = 1;  //인덱스가 초과되면 1로 변경
-    }   
-    x[index-1].style.display = "block";  //해당 인덱스는 block으로
-    setTimeout(slideShow, 2000);   //함수를 4초마다 호출
-}
+      index = 1;  //인덱스가 초과되면 1로 변경
+    }
+    x[index - 1].style.display = "block";  //해당 인덱스는 block으로
+  }
 
+  setTimeout(slideShow, 2000);   //함수를 4초마다 호출
+}
