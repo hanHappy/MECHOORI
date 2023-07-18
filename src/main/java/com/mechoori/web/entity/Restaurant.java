017ae5ceca50b7f8a26291f17bdab1ba676a154e
hanHappy <sangmin950706@gmail.com>
2023-07-18 16:25:35 +0900

    !Fix : Restaurant Entity 속성 삭제
    Top Category Id 삭제
    Category 테이블에 Top Category 칼럼 있음

--- src/main/java/com/mechoori/web/entity/Restaurant.java
+++ src/main/java/com/mechoori/web/entity/Restaurant.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Restaurant {
     private Integer id;
+	// FIXME top category id 삭제 (category 테이블에 top category id 있음)
 	private Integer topCategoryId;
 	private Integer categoryId;
 	private String name;
